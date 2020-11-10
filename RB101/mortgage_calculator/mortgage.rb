7128aad97b17f9c1ce864cda5ba35c9882af7305
Nikke Ruokolainen <nruokolainen@gmail.com>
2020-11-10 15:49:44 +0200

    Mortgage calculator refactoring

--- RB101/mortgage_calculator/mortgage.rb
+++ RB101/mortgage_calculator/mortgage.rb
@@ -2,64 +2,55 @@ require 'yaml'
 MESSAGES = YAML.load_file('messages.yml')
 
 def validate_loan_amount(loan_amount)
-  stripped_loan_amount = loan_amount.delete("$").delete(",")
-  Float(stripped_loan_amount) rescue false
+  valid_amount = loan_amount.delete("$").delete(",").to_f
+  valid_amount > 0 ? valid_amount : false
 end
 
 def validate_interest(annual_interest)
-  annual_interest.delete!("%")
-  annual_interest.gsub(",", ".")
-  Float(annual_interest) rescue false
+  valid_interest = annual_interest.delete("%").gsub(",", ".").to_f
+  valid_interest > 0 ? valid_interest : false
+end
+
+def validate_loan_duration(duration)
+  duration.to_i > 0 ? duration.to_i : false
 end
 
 def monthly_interest(apr)
-  apr/100/12
+  apr / 100 / 12
 end
 
-def duration_in_months(duration)
-  duration * 12
+def prompt(message)
+  puts MESSAGES[message]
 end
+
+puts MESSAGES['welcome']
 
 loan_amount = ''
 puts MESSAGES['ask_loan_amount']
 loop do
   loan_amount = validate_loan_amount(gets.chomp)
-
-  if loan_amount
-    break
-  else
-    puts MESSAGES['invalid_amount']
-  end
+  loan_amount ? break : prompt('invalid_amount')
 end
 
 annual_interest = ''
 puts MESSAGES['ask_interest']
 loop do
   annual_interest = validate_interest(gets.chomp)
-
-  if annual_interest
-    break
-  else
-    puts MESSAGES['invalid_interest']
-  end
+  annual_interest ? break : prompt('invalid_interest')
 end
 
 loan_duration = ''
 puts MESSAGES['ask_duration']
-
 loop do
-  loan_duration = Integer(gets.chomp) rescue false
-  if loan_duration
-    break
-  else
-    puts MESSAGES['invalid_duration']
-  end
+  loan_duration = validate_loan_duration(gets.chomp)
+  loan_duration ? break : prompt('invalid_duration')
 end
 
-monthly_payment = loan_amount * (monthly_interest(annual_interest) / (1 - (1 + monthly_interest(annual_interest))**(-duration_in_months(loan_duration))))
+monthly_payment =
+  loan_amount * (monthly_interest(annual_interest) / (1 - (1 + monthly_interest(annual_interest))**(-loan_duration * 12)))
 
 puts <<-MSG
       #{MESSAGES['monthly_payment']} $#{monthly_payment.round(2)}
       #{MESSAGES['monthly_interest']} #{(monthly_interest(annual_interest) * 100).round(2)}%
-      #{MESSAGES['duration']} #{duration_in_months(loan_duration)}
+      #{MESSAGES['duration']} #{loan_duration * 12} months.
      MSG
